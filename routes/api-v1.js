3dd19ffecfea4a3e79df50bf8c62df0cf1bc3f74
unknown <aml2732@utexas.edu>
2020-09-26 13:03:13 -0500

    Hook up individual category pages to the backend endpoint AND display the image header

--- routes/api-v1.js
+++ routes/api-v1.js
@@ -8,4 +8,21 @@ router.get('/categories', async function (req, res) {
   res.send({categories: categories.rows});
 })
 
+router.get('/category/details', async function(req, res){
+  let str = req.query.linkto;
+  if(!str){return res.status(400).send({message: 'Unrecognized category queried'})}
+  let category = await pool.query(`SELECT * FROM category WHERE LINKTO = '${str}'`);
+  if(category.rows&&category.rows.length == 1){
+    return res.send({category: category.rows[0]})
+  }else if(category.rows && category.rows.length>1){
+    return res.status(500).send({message: "More than one match for this category found. Please report this bug to the system administrator!"})
+  }else{
+    return res.status(500).send({message: "This category doesn't appear to exist."})
+  }
+});
+
+//router.get('/category/products', async function(req, res){});
+
+//router.get('/category/product/:id', async function(req, res){});
+
 module.exports = router;
--- src/Category.js
+++ src/Category.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-
 import {useParams} from 'react-router-dom'
 
 class Category extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = { error: null, isLoaded: false, category: {} };
+  }
+
+  componentDidMount() {
+    fetch("/v1/category/details?linkto=/categories/"+this.props.categoryId)
+      .then(res => res.json())
+      .then(
+        (result) => { this.setState({ isLoaded: true, category: result.category }); console.log('result', result.category) },// success handling
+        (error) => {  this.setState({ isLoaded: true, error }); console.log(error) } // Error handling
+      )
+  }
+
+
   render(){
+    let { error, isLoaded, category } = this.state;
     return (<div>
-        <h2 className="text-capitalize">{this.props.categoryId}</h2>
+      <div style={{backgroundImage: 'linear-gradient(to top right,black, transparent), url(../'+category.imgsrc+')', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height:'200px', color: 'white', position:'relative', backgroundSize: 'cover'}}>
+        <h2 className="text-capitalize" style={{position:'absolute', bottom:'2rem', textAlign: 'center', width:'100%'}}>{category.name}</h2>
+      </div>
         placeholder for products that have category:  {this.props.categoryId}
       </div>)
   }
